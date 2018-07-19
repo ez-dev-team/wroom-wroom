421e8c601534a32086a69ae117259579f4d47536
liquidprogrammer <an.kulyapin@gmail.com>
2018-07-20 01:58:04 +0500

    Keyboard input demo

--- src/demos/SAT/sat-demo.ts
+++ src/demos/SAT/sat-demo.ts
@@ -3,29 +3,102 @@ import Vector from 'engine/types/Vector'
 import Physics from 'engine/physics/physics'
 import * as GameLoop from 'engine/GameLoop'
 
+enum KeyboardCodes {
+	LEFT_ARROW = 37,
+	UP_ARROW = 38,
+	RIGHT_ARROW = 39,
+	DOWN_ARROW = 40
+}
+
+interface IKeyboardEvent {
+	key:number
+	repeat:boolean
+	ctrlKey:boolean
+	altKey:boolean
+	shiftKey:boolean
+	metaKey:boolean // TODO(kuliapin): remove? combine with ctrl key depending on OS (Win/Mac)
+}
+
+class GameObject {
+	constructor(
+		private color:string,
+		private name:string,
+		public shape:Shape) {
+	}
+
+	// TODO (kuliapin): separate function for input handling?
+	update(events:IKeyboardEvent[]) {
+		let dx = 0
+		let dy = 0
+
+		function getStep(event:IKeyboardEvent) {
+			return event.repeat ? 15 : 2 // just to move faster for demo purposes
+		}
+
+		// TODO (kuliapin): use state machine
+		events.forEach(event => {
+			if (event.key === KeyboardCodes.LEFT_ARROW) {
+				if (event.repeat) {
+				}
+				dx -= getStep(event)
+			}
+			if (event.key === KeyboardCodes.UP_ARROW) {
+				dy -= getStep(event)
+			}
+			if (event.key === KeyboardCodes.RIGHT_ARROW) {
+				dx += getStep(event)
+			}
+			if (event.key === KeyboardCodes.DOWN_ARROW) {
+				dy += getStep(event)
+			}
+		})
+
+		if (!dx && !dy) {
+			return
+		}
+
+		let vertices:Vector[] = []
+
+		this.shape.vertices.forEach(v => {
+			let newX = v.x + dx
+			let newY = v.y + dy
+			if (newX > 0 && newX < WIDTH && newY > 0 && newY < HEIGHT) {
+				vertices.push(new Vector(newX, newY))
+			}
+		})
+
+		if (vertices.length === this.shape.vertices.length) {
+			this.shape.vertices = vertices
+		}
+	}
+
+	// TODO(kuliapin): abstraction somehow
+	render(ctx:CanvasRenderingContext2D) {
+		let vertices = this.shape.vertices
+		vertices.slice(1).forEach((v, vidx) => {
+			drawLine(ctx, vertices[vidx], v, this.color, 1)
+		})
+		drawLine(ctx, vertices[0], vertices[vertices.length - 1], this.color, 1)
+	}
+}
+
 const WIDTH = 400
 const HEIGHT = 400
-const STEP_SIZE = 4 // px
 
 export function render(container:HTMLElement) {
-	let items = {
-		colors: ['#ff0000', '#00ff00'],
-		names: ['rect1', 'rect2'],
-		shapes: [
-			[new Shape([
-				new Vector(0, 0),
-				new Vector(30, 0),
-				new Vector(30, 50),
-				new Vector(0, 50)
-			].reverse())],
-			[new Shape([
-				new Vector(0, 0),
-				new Vector(30, 0),
-				new Vector(30, 50),
-				new Vector(0, 50)
-			].reverse())]
-		]
-	}
+	let objects = [
+		new GameObject('#ff0000', '#rect1', new Shape([
+			new Vector(10, 10),
+			new Vector(40, 10),
+			new Vector(40, 60),
+			new Vector(10, 60)
+		].reverse())),
+		new GameObject('#00ff00', '#rect2', new Shape([
+			new Vector(180, 170),
+			new Vector(210, 170),
+			new Vector(210, 220)
+		].reverse()))
+	]
 
 	let canvas = document.createElement('canvas')
 	canvas.width = WIDTH
@@ -37,50 +110,49 @@ export function render(container:HTMLElement) {
 	let render = () => {
 		ctx.clearRect(0, 0, WIDTH, HEIGHT)
 
-		items.shapes.forEach((shapes, idx) => {
-			stochasticCoordsUpdate(shapes)
+		objects.forEach(object => object.render(ctx))
 
-			let color = items.colors[idx]
-			shapes.forEach(shape => {
-				shape.vertices.slice(1).forEach((v, vidx) => {
-					drawLine(ctx, shape.vertices[vidx], v, color, 1)
-				})
-				drawLine(ctx, shape.vertices[0], shape.vertices[shape.vertices.length - 1], color, 1)
-			})
-		})
+		// TODO(kuliapin): physics, lol, should be before?
+		for (let i = 0; i < objects.length; i++) {
+			let obj1 = objects[i]
+			for (let j = i + 1; j < objects.length; j++) {
+				let obj2 = objects[j]
 
-		for (let i = 0; i < items.shapes.length; i++) {
-			let shapes1 = items.shapes[i]
-			for (let j = i + 1; j < items.shapes.length; j++) {
-				let shapes2 = items.shapes[j]
-
-				let anyIntersections = false
-				for (let s1 = 0; s1 < shapes1.length; s1++) {
-					let shape1 = shapes1[s1]
-					for (let s2 = 0; s2 < shapes2.length; s2++) {
-						let shape2 = shapes2[s2]
-						let mtv = Physics.SAT.getMinTranslationVector(shape1, shape2)
-						if (mtv) {
-							anyIntersections = true
-							break
-						}
-					}
-					if (anyIntersections) {
-						break
-					}
-				}
-
+				let mtv = Physics.SAT.getMinTranslationVector(obj1.shape, obj2.shape)
 				ctx.font = "10px Arial"
-				ctx.fillText(`${anyIntersections}`,WIDTH - 30,HEIGHT - 30)
+				ctx.fillText(`${!!mtv}`,WIDTH - 30,HEIGHT - 30)
 			}
 		}
 	}
 
+	let keyboardEvents:IKeyboardEvent[] = []
+
+	document.addEventListener('keydown', (event) => {
+		// TODO(kuliapin): prevent only handled events?
+		event.preventDefault()
+		event.stopPropagation()
+		event.stopImmediatePropagation()
+
+		let key = event.keyCode || event.which
+		keyboardEvents.push({
+			key: key,
+			repeat: event.repeat,
+			altKey: event.altKey,
+			ctrlKey: event.ctrlKey,
+			shiftKey: event.shiftKey,
+			metaKey: event.metaKey
+		})
+	}, true)
+
 	let tick = GameLoop.createTickFunction({
-		start: () => {},
+		start: () => {
+			let obj = objects[0]
+			obj.update(keyboardEvents.slice())
+		},
 		end: (fps) => {
 			ctx.font = "10px Arial"
 			ctx.fillText(`FPS: ${fps}`,WIDTH - 50,HEIGHT - 50)
+			keyboardEvents.length = 0
 		},
 		update: () => {},
 		render: () => render()
@@ -92,44 +164,6 @@ export function render(container:HTMLElement) {
 	}
 
 	requestAnimationFrame(animate)
-}
-
-let stepsCount = 0
-let negative = false
-
-function stochasticCoordsUpdate(shapes:Shape[]) {
-	let dx = Math.round(Math.random() * STEP_SIZE)
-	let dy = Math.round(Math.random() * STEP_SIZE)
-
-	stepsCount += negative ? -1 : 1
-	if (stepsCount > 10) {
-		negative = true
-		stepsCount = 9
-	}
-	if (stepsCount < 0) {
-		negative = false
-		stepsCount = 1
-	}
-	if (negative) {
-		dx *= -1
-		dy *= -1
-	}
-
-	shapes.forEach(shape => {
-		let vertices:Vector[] = []
-
-		shape.vertices.forEach(v => {
-			let newX = v.x + dx
-			let newY = v.y + dy
-			if (newX > 0 && newX < WIDTH && newY > 0 && newY < HEIGHT) {
-				vertices.push(new Vector(newX, newY))
-			}
-		})
-
-		if (vertices.length === shape.vertices.length) {
-			shape.vertices = vertices
-		}
-	})
 }
 
 function drawLine(ctx:CanvasRenderingContext2D, v1:Vector, v2:Vector, c:string, w:number) {
